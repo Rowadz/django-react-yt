1a1aa64dd676f603fd2791a972d9f87668593cfb
MohammedAl-rowad <rowadwo@gmail.com>
2021-06-07 01:24:55 +0300

    Add Calims to JWT (change JWT content)

--- django-api/user/urls.py
+++ django-api/user/urls.py
@@ -1,1 +1,8 @@
 from django.urls import path
+from .views import CustomTokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
+
+urlpatterns = [
+    path('login/', CustomTokenObtainPairView.as_view(), name='login'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
--- django-api/user/views.py
+++ django-api/user/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+from user.models import User
 
-# Create your views here.
+
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+
+    @classmethod
+    def get_token(cls, user: User):
+        token = super().get_token(user)
+
+        token['name'] = user.username
+        token['email'] = user.email
+        token['user_id'] = user.id
+        return token
+
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
