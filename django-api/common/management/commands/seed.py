5be9e08562a6827a8cfa234609a170d2f46675c3
MohammedAl-rowad <rowadwo@gmail.com>
2021-06-30 00:07:17 +0300

    Generate Fake users

---
+++ django-api/common/management/commands/seed.py
@@ -0,0 +1,23 @@
+from django.core.management.base import BaseCommand, CommandError
+from halo import Halo
+from user.factories import UserFactories
+
+
+class Command(BaseCommand):
+    help = 'Generate fake data and seed the models with them, default are 10'
+
+    def add_arguments(self, parser):
+        # https://docs.python.org/3/library/argparse.html#the-add-argument-method
+        # Optional!
+        parser.add_argument('--amount', type=int,
+                            help='The amount of fake data you want')
+        # parser.add_argument('amount', nargs='+', type=int)
+
+    def _generate_users(self, amount):
+        for _ in range(amount):
+            UserFactories()
+
+    @Halo(text='Generating...', spinner='dots', color='blue', text_color='blue')
+    def handle(self, *args, **options):
+        amount = options.get('amount', 10)
+        self._generate_users(amount)
---
+++ django-api/user/factories.py
@@ -0,0 +1,22 @@
+
+from factory.django import DjangoModelFactory
+from factory.faker import Faker
+from factory import Sequence, PostGenerationMethodCall, sequence
+from .models import User
+
+
+class UserFactories(DjangoModelFactory):
+    first_name = Faker('first_name')
+    last_name = Faker('last_name')
+    email = Faker('email')
+    username = Sequence(lambda n: f'User-{n}')
+    password = PostGenerationMethodCall('set_password', 'secret')
+
+    # @sequence
+    # def username(n):
+    #     max_id = User.objects.latest('id').id
+    #     return f'User-{max_id + 1}'
+
+    class Meta:
+        model = 'user.User'
+        django_get_or_create = ['username']
