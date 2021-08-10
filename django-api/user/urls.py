4c5cdb495bf630bb7fcb87a2b5b240182dcea595
MohammedAl-rowad <rowadwo@gmail.com>
2021-08-10 17:05:30 +0300

    Querying many to many and one to many

--- django-api/user/urls.py
+++ django-api/user/urls.py
@@ -13,8 +13,55 @@ urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('register/', RegisterView.as_view(), name='register'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # filter the category based on the name
     path(
-        'queries/',
+        'queries-1/',
+        ListAPIView.as_view(
+            queryset=User.objects.all().prefetch_related(
+                Prefetch(
+                    'posts__categories',
+                    queryset=Category.objects.filter(name=CategoryNames.CPP)
+                )
+            ),
+            serializer_class=UsersSerializer,
+        ),
+        name='queries'
+    ),
+    # Getting the posts that have CPP as a category
+    path(
+        'queries-2/',
+        ListAPIView.as_view(
+            queryset=User.objects.all().prefetch_related(
+                Prefetch(
+                    'posts',
+                    queryset=Post.objects.filter(
+                        categories__name__in=[CategoryNames.CPP]
+                    ).prefetch_related('categories')
+                )
+            ),
+            serializer_class=UsersSerializer,
+        ),
+        name='queries'
+    ),
+    # Filtering based on the relationship count [ONLY having a CPP as a category]
+    path(
+        'queries-3/',
+        ListAPIView.as_view(
+            queryset=User.objects.all().prefetch_related(
+                Prefetch(
+                    'posts',
+                    queryset=Post.objects.annotate(categories_count=Count('categories')).filter(
+                        categories__name=CategoryNames.CPP,
+                        categories_count=1,
+                    ).prefetch_related('categories')
+                )
+            ),
+            serializer_class=UsersSerializer,
+        ),
+        name='queries'
+    ),
+    path(
+        'queries-4/',
         ListAPIView.as_view(
             queryset=User.objects.all().prefetch_related(
                 Prefetch(
@@ -30,53 +77,3 @@ urlpatterns = [
         name='queries'
     ),
 ]
-
-# filter the category based on the name
-# path(
-#     'queries/',
-#     ListAPIView.as_view(
-#         queryset=User.objects.all().prefetch_related(
-#             Prefetch(
-#                 'posts__categories',
-#                 queryset=Category.objects.filter(name=CategoryNames.CPP)
-#             )
-#         ),
-#         serializer_class=UsersSerializer,
-#     ),
-#     name='queries'
-# ),
-
-# Getting the posts that have CPP as a category
-# path(
-#     'queries/',
-#     ListAPIView.as_view(
-#         queryset=User.objects.all().prefetch_related(
-#             Prefetch(
-#                 'posts',
-#                 queryset=Post.objects.filter(
-#                     categories__name__in=[CategoryNames.CPP]
-#                 ).prefetch_related('categories')
-#             )
-#         ),
-#         serializer_class=UsersSerializer,
-#     ),
-#     name='queries'
-# ),
-
-# Filtering based on the relationship count [ONLY having a CPP as a category]
-# path(
-#     'queries/',
-#     ListAPIView.as_view(
-#         queryset=User.objects.all().prefetch_related(
-#             Prefetch(
-#                 'posts',
-#                 queryset=Post.objects.annotate(categories_count=Count('categories')).filter(
-#                     categories__name=CategoryNames.CPP,
-#                     categories_count=1,
-#                 ).prefetch_related('categories')
-#             )
-#         ),
-#         serializer_class=UsersSerializer,
-#     ),
-#     name='queries'
-# ),
